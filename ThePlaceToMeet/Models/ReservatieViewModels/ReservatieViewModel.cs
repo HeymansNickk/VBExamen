7c53bbada84c6cbdd5d1b22a2a57e40ab9f45290
Nick Heymans <nick.heymans@hotmail.com>
2020-08-17 10:48:25 +0200

    6. ReservatieController/Reserveer HttpGet

--- ThePlaceToMeet/Models/ReservatieViewModels/ReservatieViewModel.cs
+++ ThePlaceToMeet/Models/ReservatieViewModels/ReservatieViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using ThePlaceToMeet.Models.Domain;
 
 namespace ThePlaceToMeet.Controllers
@@ -7,17 +8,31 @@ namespace ThePlaceToMeet.Controllers
     {
         public string Naam { get; set; }
 
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime Dag { get; set; }
 
+        [Required]
+        [Range(8,20)]
+        [Display(Name = "Begin uur")]
         public int BeginUur { get; set; }
 
+        [Required]
+        [Range(2,14)]
+        [Display(Name = "Hoeveel uur wil je de vergaderzaal reserveren?")]
         public int Duur { get; set; }
 
+        [Required]
+        [Display(Name = "Met hoeveel personen wil je vergaderen?")]
         public int AantalPersonen { get; set; }
 
+        [Required]
+        [Display(Name = "Standaard catering 'koffie, thee, water")]
         public bool StandaardCatering { get; set; }
 
+        [Display(Name = "Indien je ook catering wenst, selecteer wat nodig is")]
         public int CateringId { get; set; }
+
 
         public ReservatieViewModel()
         {
